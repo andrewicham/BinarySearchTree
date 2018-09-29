c1606d7c4352d762c2e679f85e414d06d23b1aac
andrewicham <andrewicham@gmail.com>
2018-09-28 22:10:23 -0400

    Changed node struct to a class.

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -7,27 +7,94 @@
 //
 #include <iostream>
 
-void createLinkList(int *A);
+//treating Node as a class, but could also have been a struct.
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node* getNewNode(int data){
+        Node* newNode = new Node();
+        newNode->data = data;
+        //newNode->left = left;
+        //newNode->right = right;
+        return newNode;
+    }
+};
+
+Node* insert(Node* root, int data);
+
 int main(int argc, const char * argv[]) {
+    Node* root = NULL;
     int k;
     int A[] = {6,17,20,41,45,52,57,65,71,76,79,87,92,95,99};
-    
+    int data;
     std::cout << "This program is to find the k-th smallest element in the binary search tree. \ndefine a value for k: ";
     std::cin >> k;
-    //createLinkList(int *A);
+    for(int i=0; i<16; i++){
+        data = A[i];
+        insert(root, data);
+        std::cout << root << " , " << data;
+    }
     return 0;
 }
 
-void createLinkList(int *A){
-    struct Node {
-        int data;
-        Node* link;
-    };
+Node* insert(Node* root, int data){
+    if(root == NULL) {
+        root->getNewNode(data);
+    }
+    else if(data <= root->data){
+        root->left = insert(root->left, data);
+    }
+    else if(data >= root->data){
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*void insert(int x){
+    
+    Node* temp = new Node();
+    temp->data = x;
+    temp->next = root;
+    root = temp;
+    
+    
+    
     for(int i=0; i<15; i++){
         Node* B;
         Node* temp = new Node();
         temp->data = A[i];
         temp->link = NULL;
         B = temp;
+        Node* temp1 = B;
+        while(temp1->link != NULL){
+            temp1 = temp1->link;
+            std::cout << temp1->data << ", ";
+            
+        }
+    }*/
+
+
+/*void print(){
+    Node* temp = head;
+    std::cout << "list is: ";
+    while(temp != NULL){
+        std::cout << temp->data << ", ";
+        temp = temp->next;
     }
-}
+}*/
