2eeead5e55ec195a40f7092d0f70317ddaed4ca9
andrewicham <andrewicham@gmail.com>
2018-10-02 21:09:51 -0400

    Created minimum, search, maximum functions

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -6,95 +6,123 @@
 //  Copyright © 2018 Andrew Chambers. All rights reserved.
 //
 #include <iostream>
+#include <cstdlib>
 
-//treating Node as a class, but could also have been a struct.
-class Node {
-public:
+struct Node{
     int data;
     Node* left;
     Node* right;
-    Node* getNewNode(int data){
-        Node* newNode = new Node();
-        newNode->data = data;
-        //newNode->left = left;
-        //newNode->right = right;
-        return newNode;
-    }
 };
 
-Node* insert(Node* root, int data);
-
-int main(int argc, const char * argv[]) {
-    Node* root = NULL;
-    int k;
-    int A[] = {6,17,20,41,45,52,57,65,71,76,79,87,92,95,99};
-    int data;
-    std::cout << "This program is to find the k-th smallest element in the binary search tree. \ndefine a value for k: ";
-    std::cin >> k;
-    for(int i=0; i<16; i++){
-        data = A[i];
-        insert(root, data);
-        std::cout << root << " , " << data;
-    }
-    return 0;
+//a constructor to set values of each new node
+Node* makeNode(int data){
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->right = NULL;
+    newNode->left = NULL;
+    return newNode;
 }
 
-Node* insert(Node* root, int data){
-    if(root == NULL) {
-        root->getNewNode(data);
+//takes in the array, 0, and the array length as input and creates a binary tree
+Node* arrayToBst(int arr[], int start, int end)
+{
+    //including this, because if the index of the array was larger than the last element, it would be nonsensical
+    if (start > end)
+    {
+        return NULL;
     }
-    else if(data <= root->data){
-        root->left = insert(root->left, data);
-    }
-    else if(data >= root->data){
-        root->right = insert(root->right, data);
-    }
+    //finding the mid value in the array as our root
+    int mid = (start + end)/2;
+    Node* root = makeNode(arr[mid]);
+    //using recursion to loop through the array, from the mid value
+    root->left = arrayToBst(arr, start, mid-1);
+    root->right = arrayToBst(arr, mid+1, end);
     return root;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*void insert(int x){
-    
-    Node* temp = new Node();
-    temp->data = x;
-    temp->next = root;
-    root = temp;
-    
-    
-    
-    for(int i=0; i<15; i++){
-        Node* B;
-        Node* temp = new Node();
-        temp->data = A[i];
-        temp->link = NULL;
-        B = temp;
-        Node* temp1 = B;
-        while(temp1->link != NULL){
-            temp1 = temp1->link;
-            std::cout << temp1->data << ", ";
-            
-        }
-    }*/
-
-
-/*void print(){
-    Node* temp = head;
-    std::cout << "list is: ";
-    while(temp != NULL){
-        std::cout << temp->data << ", ";
-        temp = temp->next;
+void inorder(Node* node)
+{
+    if(node == NULL)
+    {
+        return;
     }
-}*/
+    inorder(node->left);
+    std::cout << node->data << " ";
+    inorder(node->right);
+}
+
+//not used, but included anyways
+void maximum(Node* node)
+{
+    if(node == NULL){
+        std::cout << "empty tree" << std::endl;
+        return;
+    }
+    while(node->right != NULL){
+        node = node->right;
+    }
+    std::cout << "max: " << node->data << std::endl;
+    return;
+}
+
+Node* minimum(Node* node)
+{
+    if(node == NULL){
+        std::cout << "empty tree" <<std::endl;
+        return node;
+    }
+    while(node->left != NULL){
+        node = node->left;
+    }
+    std::cout << "min: " << node->data << std::endl;
+    return node;
+}
+
+
+int kthsmallest(Node* node, int k, int& i)
+{
+    Node* current = node;
+    int small;
+    while(current != NULL){
+        
+    }
+    
+}
+
+//not used but written anyways
+void search(Node* node, int x){
+    while(node != NULL){
+        if(x == node->data){
+            std::cout << "found!" << std::endl;
+            return;
+        }
+        else if(x < node->data){
+            node = node->left;
+        }
+        else{
+            node = node->right;
+        }
+    }
+    std::cout << "not found" << std::endl;
+    return;
+}
+
+
+
+int main(int argc, const char * argv[]) {
+
+    int k;
+    int A[] = {6,17,20,41,45,52,57,65,71,76,79,87,92,95,99};
+    std::cout << "This program is to find the k-th smallest element in the binary search tree." << std::endl;
+    std::cout << "define a value for k: ";
+    std::cin >> k;
+    
+    int n = sizeof(A)/sizeof(A[0]);
+    Node* root = arrayToBst(A, 0, n-1);
+    inorder(root);
+    Node* min = minimum(root);
+    //in order to use recursion, declaring the counter here as -1 makes sense
+    
+    std::cout << kthsmallest(min, k);
+    return 0;
+}
